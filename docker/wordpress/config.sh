81adcc957e1ba5ee19876f11e57224537ed65e0f
antoineverin <24255551+antoineverin@users.noreply.github.com>
2025-10-18 17:15:16 +0200

    add wordpress config

---
+++ docker/wordpress/config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! wp core is-installed --allow-root
+then
+	echo 'Installing Wordpress'
+	wp core install \
+		--url="$WORDPRESS_URL" \
+		--title="$WORDPRESS_TITLE" \
+		--admin_user="$WORDPRESS_ADMIN_USER" \
+		--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+		--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+		--skip-email \
+		--allow-root
+fi
